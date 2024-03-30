d979a60c2b504bd6bdcedf10915076a9f7b5a07d
carlos.alvarado <carlos.alvarado@alumnos.uneatlantico.es>
2024-03-30 23:46:52 +0100

    Implementación de Ingreso de Notas y de Muestra de Notas con Materia - Creación de switch e implementación de menu con regreso

--- Gestor_de_Calificaciones.h
+++ Gestor_de_Calificaciones.h
@@ -2,12 +2,17 @@
 #include <string>
 #include <vector>
 
+class EListException{};
+class EListOutOfRange : public EListException{};
 class Cuatrimestre1{
     private:
         std::vector<std::string> arrayDeMaterias;
-        int nota;
-    public:
+        std::string materia;
+        float nota[4];
         int promedio;
+    public:
+        float GetNota(int);
+        void SetNotas();
         void SetMaterias(std::string, std::string, std::string, std::string, std::string);
         std::vector<std::string> GetMaterias();
 }; 
--- main.cpp
+++ main.cpp
@@ -5,34 +5,72 @@
 #include "Gestor_de_Calificaciones.h"
 
 void MostrarNotas(Cuatrimestre1 *gestor);
+void IngresarNotas(Cuatrimestre1 *gestor);
+std::vector<std::string> materia;
+float notas[4];
+
+
+void Menu(){
+    std::cout << "Gestor de Calificaciones - Ingenieria Informatica" << std::endl;
+    std::cout << "(1) Ingresar Notas" << std::endl;
+    std::cout << "(2) Mostrar Notas" << std::endl;
+    std::cout << "(3) Salir del Programa" << std::endl;
+}
 
 int main(){
     int change;
     Cuatrimestre1 *gestor;
     gestor = new Cuatrimestre1();
-    std::vector<std::string> materia;
-    std::cout << "Gestor de Calificaciones - Ingenieria Informatica" << std::endl;
-    std::cout << "(1) Mostrar Notas" << std::endl;
+    gestor->SetMaterias("Matematica", "Programacion 1", "Logica", "Fisica", "Teconologias de la Informacion y Comunicacion");
+
+    do {
+    Menu();
     std::cin >> change;
-    switch (change){
-    case 1:
-        MostrarNotas(gestor);
-        break;
-    
-    default:
-        break;
-    }
+        switch (change){
+            case 1:
+                IngresarNotas(gestor);
+                break;
+            case 2:
+                MostrarNotas(gestor);
+                break;
+            case 3:
+                std::cout << "Saliendo del programa..." << std::endl;
+                break;
+            default:
+                std::cout << "Opción no válida. Por favor, seleccione nuevamente." << std::endl;
+                break;
+            }
+
+                // Preguntar al usuario si desea regresar al menú principal
+        if (change != 3) {
+            char backChoice;
+            std::cout << "" << std::endl;
+            std::cout << "Desea volver al menu principal (S/N): ";
+            std::cin >> backChoice;
+            if (backChoice != 'S' && backChoice != 's') {
+                change = 3; // Salir del bucle si no desea volver al menú
+            }
+        }
+
+    } while (change != 3);
 
     delete gestor;
     return 0;
 }
 
 void MostrarNotas(Cuatrimestre1 *gestor){
-    std::vector<std::string> materia;
-    gestor->SetMaterias("Matematica", "Programacion 1", "Logica", "Fisica", "Teconologias de la Informacion y Comunicacion");
     materia = gestor->GetMaterias();
     std::cout << "Materias del Primer Cuatrimestre: " << std::endl;
     for (int i = 0; i < materia.size(); i++){
-        std::cout << "Materia: " << materia[i] << std::endl;
+        std::cout << "Materia: " << materia[i] << " ------------- " << " Nota: " << notas[i] << std::endl;
     }
+}
+
+void IngresarNotas(Cuatrimestre1 *gestor){
+    gestor->SetNotas();
+    notas[0] = gestor->GetNota(0);
+    notas[1] = gestor->GetNota(1);
+    notas[2] = gestor->GetNota(2);
+    notas[3] = gestor->GetNota(3);
+    notas[4] = gestor->GetNota(4);
 }
