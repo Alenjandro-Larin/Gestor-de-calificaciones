9af14ba25682b4afe97fd909821a33dcd42d0bb8
miguel.rivas <miguel.rivas@alumnos.uneatlantico.es>
2024-04-13 20:15:08 +0200

    fix - Error al mostrar las notas - 14-04-2024

--- Gestor-de-calificaciones/mostrarnotas.cpp
+++ Gestor-de-calificaciones/mostrarnotas.cpp
@@ -14,27 +14,27 @@ MostrarNotas::MostrarNotas(QWidget *parent)
     ui->setupUi(this);
 
 
-    if (nota[0] < 10){
+    if (nota[0] <= 10){
         ui->label_matematicaI->setNum(nota[0]);
     }else{
         ui->label_matematicaI->setText("- Esa calificación no es posible.");
     }
-    if (nota[1] < 10){
+    if (nota[1] <= 10){
         ui->label_programacionI->setNum(nota[1]);
     }else{
         ui->label_programacionI->setText("- Esa calificación no es posible.");
     }
-    if (nota[2] < 10){
+    if (nota[2] <= 10){
         ui->label_introduccion->setNum(nota[2]);
     }else{
         ui->label_introduccion->setText("- Esa calificación no es posible.");
     }
-    if (nota[3] < 10){
+    if (nota[3] <= 10){
         ui->label_fisica->setNum(nota[3]);
     }else{
         ui->label_fisica->setText("- Esa calificación no es posible.");
     }
-    if (nota[5] < 10){
+    if (nota[5] <= 10){
         ui->label_teo->setNum(nota[5]);
     }else{
         ui->label_teo->setText("- Esa calificación no es posible.");
@@ -47,27 +47,27 @@ MostrarNotas::MostrarNotas(QWidget *parent)
     }
 
 
-    if (nota2[0] < 10){
+    if (nota2[0] <= 10){
         ui->label_matematicaII->setNum(nota2[0]);
     }else{
         ui->label_matematicaII->setText("- Esa calificación no es posible.");
     }
-    if (nota2[1] < 10){
+    if (nota2[1] <= 10){
         ui->label_programacionII->setNum(nota2[1]);
     }else{
         ui->label_programacionII->setText("- Esa calificación no es posible.");
     }
-    if (nota2[2] < 10){
+    if (nota2[2] <= 10){
         ui->label_logica->setNum(nota2[2]);
     }else{
         ui->label_logica->setText("- Esa calificación no es posible.");
     }
-    if (nota2[3] < 10){
+    if (nota2[3] <= 10){
         ui->label_matematicadiscreta->setNum(nota2[3]);
     }else{
         ui->label_matematicadiscreta->setText("- Esa calificación no es posible.");
     }
-    if (nota2[4] < 10){
+    if (nota2[4] <= 10){
         ui->label_tic->setNum(nota2[4]);
     }else{
         ui->label_tic->setText("- Esa calificación no es posible.");
