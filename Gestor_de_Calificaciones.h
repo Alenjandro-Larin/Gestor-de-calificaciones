b884d938d306607aa7fa5dd5580acbabb92e333a
alejandro-larin <program.alex.html@gmail.com>
2024-04-15 12:50:18 +0200

    Actualizacion de Cuatrimestre 2

--- Gestor_de_Calificaciones.h
+++ Gestor_de_Calificaciones.h
@@ -1,36 +1,17 @@
 #include <iostream>
-#include <string>
-#include <vector>
 
-struct Estructura{
-    float nota[5];
-    float promedio;
-};
-typedef struct Estructura TAsignatura;
-class Cuatrimestre1{
-    private:
-        std::vector<std::string> arrayDeMaterias;
-        std::string materias;
-        TAsignatura asignaturasPrimerSemestre;
-    public:
-        float GetNota(int);
-        float GetPromedio();
-        void SetNotas();
-        void SetPromedio();
-        void SetMaterias(std::string, std::string, std::string, std::string, std::string);
-        std::vector<std::string> GetMaterias();
+class Cuatrimestre2
+{
+private:
+    /* data */
+public:
+    // data
 };
 
-class Cuatrimestre2{
-    private:
-        std::vector<std::string> arrayDeMaterias2;
-        std::string materias;
-        TAsignatura asignaturasSegundoSemestre;
-    public:
-        float GetNotaSemestre2(int);
-        float GetPromedioSemestre2();
-        void SetNotasSemestre2();
-        void SetPromedioSemestre2();
-        void SetMateriasSemestre2(std::string, std::string, std::string, std::string, std::string);
-        std::vector<std::string> GetMateriasSemestre2();
-};
+Cuatrimestre2::Cuatrimestre2(/* args */)
+{
+}
+
+Cuatrimestre2::~Cuatrimestre2()
+{
+}
