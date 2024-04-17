296e552f92e4071810712e5b8b17997348b2a1e5
alejandro-larin <program.alex.html@gmail.com>
2024-04-17 22:14:05 +0200

    Implementacion de nuevas funcionalidades de la rama Alejandro-larin

--- Gestor_de_Calificaciones.h
+++ Gestor_de_Calificaciones.h
@@ -1,36 +1,45 @@
+#pragma once // Directiva de preprocesador para evitar la inclusión múltiple del archivo de encabezado
+
 #include <iostream>
 #include <string>
 #include <vector>
 
-struct Estructura{
-    float nota[5];
+using namespace std;
+
+struct Estructura {
+    vector<float> arrayDeNotas; // Cambiado de arrayDeNota a arrayDeNotas
     float promedio;
 };
-typedef struct Estructura TAsignatura;
-class Cuatrimestre1{
-    private:
-        std::vector<std::string> arrayDeMaterias;
-        std::string materias;
-        TAsignatura asignaturasPrimerSemestre;
-    public:
-        float GetNota(int);
-        float GetPromedio();
-        void SetNotas();
-        void SetPromedio();
-        void SetMaterias(std::string, std::string, std::string, std::string, std::string);
-        std::vector<std::string> GetMaterias();
-};
 
+class Cuatrimestre1 {
+private:
+    vector<string> arrayDeMaterias;
+    // string materias; // No necesitas esta variable miembro
+    Estructura asignaturasPrimerSemestre; // Cambiado de TAsignatura a Estructura
+
+public:
+    // Métodos Set
+    void SetNotas();
+    void SetPromedio(); // No necesitas este método aquí si no lo estás utilizando
+    void SetMaterias(const vector<string>& materias); // Cambiado el tipo de retorno y nombre del parámetro
+
+    // Métodos Get
+    float GetNota(int index); // Cambiado el tipo de retorno y agregado el parámetro index
+    float GetPromedio();
+    vector<string> GetMaterias();
+};//end of the class
+
+/*
 class Cuatrimestre2{
     private:
-        std::vector<std::string> arrayDeMaterias2;
-        std::string materias;
+        vector<string> arrayDeMaterias2;
+        string materias;
         TAsignatura asignaturasSegundoSemestre;
     public:
         float GetNotaSemestre2(int);
         float GetPromedioSemestre2();
         void SetNotasSemestre2();
         void SetPromedioSemestre2();
-        void SetMateriasSemestre2(std::string, std::string, std::string, std::string, std::string);
-        std::vector<std::string> GetMateriasSemestre2();
-};
+        void SetMateriasSemestre2(string, string, string, string, string);
+        vector<string> GetMateriasSemestre2();
+};*/
--- main.cpp
+++ main.cpp
@@ -1,64 +1,50 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <limits>
 #include "Gestor_de_Calificaciones.h"
-
-void MostrarPromedio(Cuatrimestre1 *gestor);
-void MostrarNotas(Cuatrimestre1 *gestor);
-void IngresarNotas(Cuatrimestre1 *gestor);
-
-void MostrarPromedioSemestre2(Cuatrimestre2 *gestor2);
-void MostrarNotasSemestre2(Cuatrimestre2 *gestor2);
-void IngresarNotasSemestre2(Cuatrimestre2 *gestor2);
-
-void PromedioTotal(Cuatrimestre1 *gestor, Cuatrimestre2 *gestor2);
-std::vector<std::string> materia;
-std::vector<std::string> materiaSegundoSemestre;
-float notas[5];
-float notas2[4];
-
+using namespace std;
 
 void Menu(){
-    std::cout << "Gestor de Calificaciones - Ingenieria Informatica -" << std::endl;
-    std::cout << "(1) Ingresar Notas" << std::endl;
-    std::cout << "(2) Mostrar Notas" << std::endl;
-    std::cout << "(3) Promedio Total del Curso"  << std::endl;
-    std::cout << "(4) Salir del Programa" << std::endl;
+    cout << "Gestor de Calificaciones - Ingenieria Informatica -" << endl;
+    cout << "(1) Ingresar Notas" << endl;
+    cout << "(2) Mostrar Notas" << endl;
+    cout << "(3) Promedio Total del Curso"  << endl;
+    cout << "(4) Salir del Programa" << endl;
+    while (!(cin >> change)) {
+    cout << "Entrada invalida. Ingrese un numero: ";
+    cin.clear(); 
+    cin.ignore(numeric_limits<streamsize>::max(), '\n'); 
 }
 
 void Menu2(){
-    std::cout << "(A) Cuatrimestre 1" << std::endl;
-    std::cout << "(B) Cuatrimestre 2" << std::endl;
+    cout << "(A) Cuatrimestre 1" << endl;
+    cout << "(B) Cuatrimestre 2" << endl;
 }
 
 int main(){
     int change;
-    char change2;
-    Cuatrimestre1 *gestor;
-    Cuatrimestre2 *gestor2;
-    gestor = new Cuatrimestre1();
-    gestor2 = new Cuatrimestre2();
-    gestor->SetMaterias("Matematica I", "Programacion I", "Introduccion a la Gestion de Proyectos de Software", "Fisica", "Teconologia y Estructura de Ordenadores");
-    gestor2->SetMateriasSemestre2("Matematica II", "Programacion II", "Logica", "Matematica Discreta", "Tecnologias de la Informacion y Comunicacion");
+    int change2;
+    Cuatrimestre1 cuatrimestre1;
+    // Suponiendo que tienes una instancia de Cuatrimestre1 llamada cuatrimestre1
+    vector<string> materias = {"Matematica I", "Programacion I", "Introduccion a la Gestion de Proyectos de Software", "Fisica", "Teconologia y Estructura de Ordenadores"};
+    cuatrimestre1.SetMaterias(materias);
+
     do {
     Menu();
-    while (!(std::cin >> change)) {
-    std::cout << "Entrada invalida. Ingrese un numero: ";
-    std::cin.clear(); 
-    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); 
     }
         switch (change){
             case 1:
                 Menu2();
-            std::cin >> change2;
+            cin >> change2;
             change2 = tolower(change2);
                 switch (change2)
                 {
                 case 'a':
-                    IngresarNotas(gestor);
+                    cuatrimestre1.SetNotas();
                     break;
                 case 'b':
-                    IngresarNotasSemestre2(gestor2);
+     
                     break;
                 default:
                     break;
@@ -66,40 +52,37 @@ int main(){
                 break;
             case 2:
                 Menu2();
-            std::cin >> change2;
+            cin >> change2;
             change2 = tolower(change2);
                 switch (change2)
                 {
                 case 'a':
-                    MostrarNotas(gestor);
-                    std::cout << std::endl;
-                    MostrarPromedio(gestor);
+                    // MostrarNotas(gestor);
+                    // cout << endl;
+                    // MostrarPromedio(gestor);
                     break;
                 case 'b':
-                    MostrarNotasSemestre2(gestor2);
-                    std::cout << std::endl;
-                    MostrarPromedioSemestre2(gestor2);
                 default:
                     break;
                 }
                 break;
             case 3:
-                PromedioTotal(gestor, gestor2);
+                // PromedioTotal(gestor, gestor2);
                 break;
             case 4:
-                std::cout << "Saliendo del programa..." << std::endl;
+                cout << "Saliendo del programa..." << endl;
                 break;
             default:
-                std::cout << "Opcion no valida. Por favor, seleccione nuevamente." << std::endl;
+                cout << "Opcion no valida. Por favor, seleccione nuevamente." << endl;
                 break;
             }
 
         if (change != 4) {
             char backChoice;
-            std::cout << "" << std::endl;
-            std::cout << "Desea volver al menu principal (S/N): ";
-            std::cin >> backChoice;
-            std::cout << std::endl;
+            cout << "" << endl;
+            cout << "Desea volver al menu principal (S/N): ";
+            cin >> backChoice;
+            cout << endl;
             if (backChoice != 'S' && backChoice != 's') {
                 change = 4; 
             }
@@ -107,66 +90,59 @@ int main(){
 
     } while (change != 4);
 
-    delete gestor;
-    delete gestor2;
+
     return 0;
-}
+} 
 
-void MostrarNotas(Cuatrimestre1 *gestor){
-    materia = gestor->GetMaterias();
-    std::cout << "Materias del Primer Semestre: " << std::endl;
-    for (int i = 0; i < materia.size(); i++){
-        std::cout << "Materia: " << materia[i] << " ------------- " << " Nota: " << notas[i] << std::endl;
-    }
-}
-
-void IngresarNotas(Cuatrimestre1 *gestor){
-    gestor->SetNotas();
-    for (int i = 0; i < 5; i++) {
-        notas[i] = gestor->GetNota(i);
-    }
-}
-
-void MostrarPromedio(Cuatrimestre1 *gestor){
-    gestor->SetPromedio();
-    std::cout << "El Promedio Total del Semestre fue de: " << gestor->GetPromedio() << std::endl;
-}
-
-void MostrarPromedioSemestre2(Cuatrimestre2 *gestor2){
-    gestor2->SetPromedioSemestre2();
-    std::cout << "El Promedio Total del Semestre fue de: " << gestor2->GetPromedioSemestre2() << std::endl;
-   
-}
-
-void MostrarNotasSemestre2(Cuatrimestre2 *gestor2)
-{
-    materiaSegundoSemestre = gestor2->GetMateriasSemestre2(); 
-    std::cout << "Materias del Segundo Semestre: " << std::endl;
-    for (int i = 0; i < materiaSegundoSemestre.size(); i++){
-        std::cout << "Materia: " << materiaSegundoSemestre[i] << " ------------- " << " Nota: " << notas2[i] << std::endl;
-    }
-}
-
-void IngresarNotasSemestre2(Cuatrimestre2 *gestor2){
-    gestor2->SetNotasSemestre2();
-    for (int i = 0; i < 5; i++){
-        notas2[i] = gestor2->GetNotaSemestre2(i);
-    }
-}
-
-void PromedioTotal(Cuatrimestre1 *gestor, Cuatrimestre2 *gestor2){
-
-    if (gestor->GetPromedio() == 0 || gestor2->GetPromedioSemestre2() == 0){
-        std::cout << "AVISO: No hay registro de alguno de los semestres todavia" << std::endl;
-    }
-    else{
-        float promedioTotal = (gestor->GetPromedio() + gestor2->GetPromedioSemestre2())/2;
-        std::cout << "El promedio total de los 2 semestres fue de: " << promedioTotal << std::endl;
-        if (promedioTotal > 8){
-            std::cout << "Felicidades obtuviste una matricula de honor!!" << std::endl;
-        }
-        else if (promedioTotal <= 4){
-            std::cout << "Lastimosamente has reprobado" << std::endl;
-        }
-    }
-}
+/*
+ void MostrarNotas(Cuatrimestre1 *gestor){
+     materia = gestor->GetMaterias();
+     cout << "Materias del Primer Semestre: " << endl;
+     for (int i = 0; i < materia.size(); i++){
+         cout << "Materia: " << materia[i] << " ------------- " << " Nota: " << notas[i] << endl;
+     }
+ }
+ void IngresarNotas(Cuatrimestre1 *gestor){
+     gestor->SetNotas();
+     for (int i = 0; i < 5; i++) {
+         notas[i] = gestor->GetNota(i);
+     }
+ }
+ void MostrarPromedio(Cuatrimestre1 *gestor){
+     gestor->SetPromedio();
+     cout << "El Promedio Total del Semestre fue de: " << gestor->GetPromedio() << endl;
+ }
+ void MostrarPromedioSemestre2(Cuatrimestre2 *gestor2){
+     gestor2->SetPromedioSemestre2();
+     cout << "El Promedio Total del Semestre fue de: " << gestor2->GetPromedioSemestre2() << endl;
+ 
+ }
+ void MostrarNotasSemestre2(Cuatrimestre2 *gestor2)
+ {
+     materiaSegundoSemestre = gestor2->GetMateriasSemestre2(); 
+     cout << "Materias del Segundo Semestre: " << endl;
+     for (int i = 0; i < materiaSegundoSemestre.size(); i++){
+         cout << "Materia: " << materiaSegundoSemestre[i] << " ------------- " << " Nota: " << notas2[i] << endl;
+     }
+ }
+ void IngresarNotasSemestre2(Cuatrimestre2 *gestor2){
+     gestor2->SetNotasSemestre2();
+     for (int i = 0; i < 5; i++){
+         notas2[i] = gestor2->GetNotaSemestre2(i);
+     }
+ }
+ void PromedioTotal(Cuatrimestre1 *gestor, Cuatrimestre2 *gestor2){
+     if (gestor->GetPromedio() == 0 || gestor2->GetPromedioSemestre2() == 0){
+         cout << "AVISO: No hay registro de alguno de los semestres todavia" << endl;
+     }
+     else{
+         float promedioTotal = (gestor->GetPromedio() + gestor2->GetPromedioSemestre2())/2;
+         cout << "El promedio total de los 2 semestres fue de: " << promedioTotal << endl;
+         if (promedioTotal > 8){
+             cout << "Felicidades obtuviste una matricula de honor!!" << endl;
+         }
+         else if (promedioTotal <= 4){
+             cout << "Lastimosamente has reprobado" << endl;
+         }
+     }
+ }*/
